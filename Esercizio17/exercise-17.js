3148280864da28d0780b10231c11ea4d7d7398cd
Emanuele Braco <emanuele.braco@gmail.com>
2022-04-20 18:54:31 +0200

    feat: i did exercise 17

--- Esercizio17/exercise-17.js
+++ Esercizio17/exercise-17.js
@@ -1,6 +1,30 @@
-function calculate() {
-  // ...
+function calculate() { //implementiamo la funzione
+  return { //restituisce l'oggetto
+    totale: 0,
+    add: function (num) {
+      this.totale += num; //this.totale prendo l'istanza di quella proprietà//
+      return this; //così ritorno tutta l'istanza dell'oggetto//
+    },
+    sub: function (num) {
+      this.totale -= num;
+      return this;
+    },
+    multiply: function (num) {
+      this.totale *= num;
+      return this;
+    },
+    divide: function (num) {
+      this.totale /= num;
+      return this;
+    },
+    printResult: function () {
+      return this.totale; //ritornerà il totale//
+    },
+  };
 }
 
 const calculator = calculate();
-calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); // Il risultato sarà: 7
+//salviamo il valore del totale in questa varabile 'result'//
+const result = calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); 
+// Il risultato sarà: 7
+console.log(result);
