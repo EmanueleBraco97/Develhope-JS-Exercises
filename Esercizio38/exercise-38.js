5a422fe9540be059998dfecf8239f9660fb9f3df
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-02 18:36:09 +0200

    feat: i did exercise 38

--- Esercizio38/exercise-38.js
+++ Esercizio38/exercise-38.js
@@ -2,6 +2,9 @@ class Square {
   constructor(side) {
     this.side = side;
   }
+  getArea(){
+    return this.side* this.side;
+  }
 }
 
 class Rectangle {
@@ -9,17 +12,31 @@ class Rectangle {
     this.width = width;
     this.height = height;
   }
+  getArea(){
+    return this.width*this.height;
+  }
 }
 
 class Circle {
   constructor(radius) {
     this.radius = radius;
   }
+  getArea(){
+    return this.radius* this.radius;
+  }
 }
 
 class AreaCalculator {
-  // ...
+  static calculate(object){
+    if(object instanceof Square || object instanceof Rectangle || object instanceof Circle){
+      return object.getArea();
+    }else{
+      return 'istanza non definita';
+    }
+  }
 }
+
+
 
 const square = new Square(4);
 const rectangle = new Rectangle(4, 2);
