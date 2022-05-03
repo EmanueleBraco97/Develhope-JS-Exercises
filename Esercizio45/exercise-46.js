c5c68b18561389a195eba3ae469165ed9adf5560
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-03 13:17:27 +0200

    feat: i did exercise 46

---
+++ Esercizio45/exercise-46.js
@@ -0,0 +1,19 @@
+// function isAdult(person) {
+//   return person.age >= 18;
+// }
+
+function isAdult(person){
+  let{age}=person;
+  return age>18;
+
+}
+
+
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+console.log(isAdult(person));
