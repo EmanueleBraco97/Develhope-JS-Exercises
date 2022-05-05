cea8cbfd149efe30affaba20f1db37ea8135bed1
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-05 16:56:55 +0200

    feat: bugfix exercise 53

--- Esercizio52/exercise-53.js
+++ Esercizio52/exercise-53.js
@@ -2,7 +2,6 @@ function sum(...args) {
     console.log(args)
     // return num1 + num2 + num3 + num4 + num5;
     let total = 0;
-
     args.forEach(arg => { //HO CALCOLATO ANCHE LA SOMMA//
        total+=arg;
     });
