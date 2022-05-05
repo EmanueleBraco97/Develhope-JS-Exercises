a682d34bd1ed846779f2b1f1f71560754aa74793
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-05 16:56:22 +0200

    feat: i did exercise 53 and 54

---
+++ Esercizio52/exercise-53.js
@@ -0,0 +1,12 @@
+function sum(...args) {
+    console.log(args)
+    // return num1 + num2 + num3 + num4 + num5;
+    let total = 0;
+
+    args.forEach(arg => { //HO CALCOLATO ANCHE LA SOMMA//
+       total+=arg;
+    });
+    return total;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
