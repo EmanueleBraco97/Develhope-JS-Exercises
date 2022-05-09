ca603377960a9d917b89954c8e0855966cacd376
Emanuele Braco <emanuele.braco@gmail.com>
2022-05-09 12:48:16 +0200

    feat: i did exercise 58

--- Esercizio58/exercise-58.js
+++ Esercizio58/exercise-58.js
@@ -20,9 +20,17 @@ const persons = [
 ];
 
 function fetchPersonById(id) {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(persons.find(item => item.id === id)), 1000);
+  return new Promise((resolve, reject) => { 
+    setTimeout(() => {
+      if(resolve(persons.find(item => item.id === id))){
+        resolve (id);
+      }else{
+        reject(new Error('non esiste una persona con id passato come parametro'))
+      }
+      resolve(id)
+    }, 1000);
   });
 }
+
 
 fetchPersonById(2).then((person) => console.log(person));
